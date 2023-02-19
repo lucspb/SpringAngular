f773de5f5bc5b3ba07c97315995508efeae4d60b
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-02-19 11:32:44 -0300

    adicionando classe de dominio

---
+++ src/main/java/com/lucas/fullstack/domain/Todo.java
@@ -0,0 +1,98 @@
+package com.lucas.fullstack.domain;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Todo implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dataParaFinalizar;
+	private boolean finalizado = false;
+
+	public Todo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Todo(Integer id, String titulo, String descricao, LocalDateTime dataParaFinalizar, boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataParaFinalizar = dataParaFinalizar;
+		this.finalizado = finalizado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDataParaFinalizar() {
+		return dataParaFinalizar;
+	}
+
+	public void setDataParaFinalizar(LocalDateTime dataParaFinalizar) {
+		this.dataParaFinalizar = dataParaFinalizar;
+	}
+
+	public boolean isFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}

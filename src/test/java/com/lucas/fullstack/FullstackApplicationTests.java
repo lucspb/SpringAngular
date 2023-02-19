0f9c7569fe54b85b5def9f2fa3586282410b855b
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-02-19 11:11:25 -0300

    primeiro commit

---
+++ src/test/java/com/lucas/fullstack/FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
